f87eeebde2f0d6bcf29d421fe3dbd6007c4bfb50
Fer Aguirre <kay_op@protonmail.com>
2022-03-04 17:22:43 -0600

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Escritoras Latinas',
+    version='0.1.0',
+    author='Fer Aguirre',
+    description='Web scraping of wikipedia entries for Latin American women writers and graph visualization.',
+    python_requires='>=3',
+    license='MIT License',
+    packages=find_packages(),
+)
